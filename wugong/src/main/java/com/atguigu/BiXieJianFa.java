b3c214d40f268a4d9b7f349b83b9b76d04c811ce
ouwen <ouwen222@qq.com>
2023-11-13 20:54:31 +0800

    创建 辟邪剑法

---
+++ wugong/src/main/java/com/atguigu/BiXieJianFa.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+public class BiXieJianFa {
+    public static void main(String[] args) {
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+    }
+
+}
