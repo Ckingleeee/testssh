d732fefaedfdebe507bdf0180225066486970fea
yyf <yyf@atguigu.com>
2023-11-13 21:03:10 +0800

    创建

---
+++ wugong/src/main/java/com/atguigu/DuGuJiuJian.java
@@ -0,0 +1,11 @@
+package wugong.src.main.java.com.atguigu;
+
+public class DuGuJiuJian {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+    }
+}
